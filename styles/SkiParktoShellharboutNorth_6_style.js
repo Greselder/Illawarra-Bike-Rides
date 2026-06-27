7a0c2c2c912f2faf3aae143fd5d298d1cc963e27
Illawarra Bike Rides <ken_telfer@live.com.au>
2026-06-27 17:04:46 +1000

    Add files via upload

---
+++ styles/SkiParktoShellharboutNorth_6_style.js
@@ -0,0 +1,65 @@
+var size = 0;
+var placement = 'point';
+function categories_SkiParktoShellharboutNorth_6(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'blue':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(31,120,180,1.0)', lineDash: null, lineCap: 'round', lineJoin: 'round', width: 4.028}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'oragne':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(244,137,222,1.0)', lineDash: null, lineCap: 'round', lineJoin: 'round', width: 4.028}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'yellow':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(234,240,22,1.0)', lineDash: null, lineCap: 'round', lineJoin: 'round', width: 4.028}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(230,92,115,1.0)', lineDash: null, lineCap: 'round', lineJoin: 'round', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_SkiParktoShellharboutNorth_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Colour");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_SkiParktoShellharboutNorth_6(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
